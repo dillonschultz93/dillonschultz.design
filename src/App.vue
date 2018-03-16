7847ce4df6a21275fb300e0a055a05e3fc5f4b98
Dillon Schultz <dillonschultz93@gmail.com>
2018-03-16 01:14:03 -0700

    Added routes

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,39 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Navbar />
+    <transition name="fade" mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
 <script>
+import Navbar from './components/navbar';
+
 export default {
   name: 'App',
+  components: {
+    Navbar,
+  },
+  beforeRouteUpdate(to, from, next) {
+    const toDepth = to.path.split('/').length;
+    const fromDepth = from.path.split('/').length;
+    this.transitionName = toDepth < fromDepth ? 'slide-right' : 'slide-left';
+    next();
+  },
 };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  #app {
+    height: 96vh;
+  }
+  // ANIMATIONS
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 1s, transform 1s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+    transform: translateX(30%);
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Home from '@/components/home';
+import About from '@/components/about';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
+  base: __dirname,
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About,
     },
   ],
 });
