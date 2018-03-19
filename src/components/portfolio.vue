bd67dbb6c7b7f96b354a9ba501788c46c0e38c16
Dillon Schultz <dillonschultz93@gmail.com>
2018-03-18 19:23:37 -0700

    Created the portfolio component

--- src/components/portfolio.vue
+++ src/components/portfolio.vue
@@ -1,21 +1,20 @@
 <template>
   <div id="portfolio">
     <h1 class="heading">Portfolio</h1>
-    <div class="portfolio-container"
-    v-for="(item, index) in portfolio_items"
-    :key="item.id">
-      <div class="image-container">
-        <img :src="item.img_src" alt="portfolio item image">
-      </div>
-      <div class="text-container"
-      @mouseenter="mouseEnter(index)"
-      @mouseleave="mouseLeave(index)">
-        <p class="project-title">{{ item.portfolio_name }}</p>
-        <p class="project-type">{{ item.portfolio_type }}</p>
-        <transition name="fadeUp" mode="out-in">
-          <p class="project-description" v-if="item.active">{{ item.description }}</p>
-        </transition>
-      </div>
+    <div class="portfolio">
+      <router-link :to="{ name: item.route}" class="portfolio-container"
+      v-for="(item, index) in portfolio_items"
+      :key="item.id">
+        <div class="image-container">
+          <div class="content-overlay"></div>
+          <img :src="getPics(index)" alt="portfolio item image" class="project-image">
+          <div class="info-container fadeIn-bottom">
+            <p class="project-name">{{ item.portfolio_name }}</p>
+            <div class="shape"></div>
+            <p class="project-description">{{ item.description }}</p>
+          </div>
+        </div>
+      </router-link>
     </div>
   </div>
 </template>
@@ -32,15 +31,15 @@ export default {
           description: `PolitiVue is a simple app that displays local and federal
           government officials, their party affiliation, contact information,
           social media accounts, and where their offices are located.`,
-          img_src: 'http://placehold.it/350x350',
-          active: false,
+          image: 'politivue',
+          route: 'politivue',
         },
         {
-          portfolio_name: 'dillonschultz.design',
+          portfolio_name: 'Personal Portfolio',
           portfolio_type: 'Front-End',
           description: 'The process and design of this very portfolio website.',
-          img_src: 'http://placehold.it/350x350',
-          active: false,
+          image: 'portfolio',
+          route: '',
         },
         {
           portfolio_name: 'ReelVue',
@@ -48,8 +47,8 @@ export default {
           description: `ReelVue is a  web app that  provides information to movie
           fans regarding their favorite movies and movies that are currently
           playing in theaters`,
-          img_src: 'http://placehold.it/350x350',
-          active: false,
+          image: 'ReelVue',
+          route: '',
         },
         {
           portfolio_name: 'Center Pride',
@@ -57,39 +56,24 @@ export default {
           description: `A series of posters and flyers promoting a Pride Week event
           at hosted in the Arcadia housing community, located on Arizona State's 
           Tempe location.`,
-          img_src: 'http://placehold.it/350x350',
-          active: false,
-        },
-        {
-          portfolio_name: 'Logo Design',
-          portfolio_type: 'Graphic Design',
-          description: 'A compilation of various logos and brandmarks I have designed.',
-          img_src: 'http://placehold.it/350x350',
-          active: false,
+          image: 'center-pride',
+          route: '',
         },
         {
           portfolio_name: 'Editorial Design',
           portfolio_type: 'Graphic Design',
           description: `A compilation of various layouts designs for single page
           use, or for full publications.`,
-          img_src: 'http://placehold.it/350x350',
-          active: false,
+          image: 'editorial',
+          route: '',
         },
         {
           portfolio_name: 'Illustrations',
           portfolio_type: 'Graphic Design',
           description: `A compilation of various illustrations I have done over
           the years.`,
-          img_src: 'http://placehold.it/350x350',
-          active: false,
-        },
-        {
-          portfolio_name: 'Photography',
-          portfolio_type: 'Photography',
-          description: `A compilation of various photographs I have captured over
-          the years.`,
-          img_src: 'http://placehold.it/350x350',
-          active: false,
+          image: 'illustration',
+          route: '',
         },
       ],
     };
@@ -105,6 +89,10 @@ export default {
         this.portfolio_items[index].active = false;
       }
     },
+    getPics(index) {
+      const images = require.context('../assets/', false, /\.jpg$/);
+      return images(`./${this.portfolio_items[index].image}.jpg`);
+    },
   },
 };
 </script>
@@ -112,8 +100,11 @@ export default {
 <style lang="scss" scoped>
   @import "../assets/scss/styles.scss";
   #portfolio{
-    lost-center: 1200px;
     margin-top: 15vh;
+  }
+  .portfolio{
+    lost-center: 1200px;
+    lost-utility: clearfix;
   }
   .heading{
     color: $primary-shade;
@@ -122,53 +113,90 @@ export default {
     font-weight: $heavy-weight;
     margin-bottom: 80px;
     margin-top: 0;
+    lost-center: 1200px;
   }
   .portfolio-container{
-    align-items: center;
-    display: flex;
-    lost-column: 1/1;
+    lost-column: 1/3;
     margin-bottom: 20px;
-    margin-top: 20px;
-    .image-container{
-      lost-column: 1/3;
-      img{
-        border-radius: 8px;
-        height: 100%;
-        width: 100%;
-      }
-    }
-    .text-container{
-      lost-column: 2/3;
-      .project-title{
-        color: $primary-shade;
-        font-family: $heading-font;
-        font-size: $heading-2-size;
-        font-weight: $heavy-weight;
-        line-height: $primary-leading;
-        margin-bottom: 10px;
-      }
-      .project-type{
-        color: $primary-shade-2;
-        font-family: $primary-font;
-        font-size: $paragraph;
-        font-weight: $light-weight;
-        line-height: $primary-leading;
-      }
-      .project-description{
-        color: $primary-shade;
-        font-family: $primary-font;
-        font-size: $paragraph;
-        font-weight: $light-weight;
-        line-height: $primary-leading;
-      }
+    box-sizing: border-box;
+  }
+  .project-image{
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+  }
+  .image-container{
+    position: relative;
+    .content-overlay{
+      background: rgba(88,124,163,0.85);
+      position: absolute;
+      height: 99%;
+      width: 100%;
+      left: 0;
+      top: 0;
+      bottom: 0;
+      right: 0;
+      opacity: 0;
+      transition: all 0.4s ease-in-out 0s;
+      border-radius: 10px;
     }
   }
+  .image-container:hover .content-overlay{
+    opacity: 1;
+  }
+  .info-container{
+    color: $secondary-color;
+    position: absolute;
+    text-align: center;
+    width: 80%;
+    top: 50%;
+    left: 50%;
+    opacity: 0;
+    transform: translate(-50%, -50%);
+    transition: all 0.3s ease-in-out 0s;
+    border-radius: 10px;
+    .project-name{
+      font-family: $primary-font;
+      font-size: $heading-3-size;
+      font-weight: $heavy-weight;
+      text-align: left;
+      margin: 0;
+    }
+    .shape{
+      width: 4em;
+      height: 0.25em;
+      background-color: $secondary-color;
+      margin: 8px 0 8px 0;
+    }
+    .project-description{
+      font-family: $primary-font;
+      font-size: $paragraph;
+      font-weight: $light-weight;
+      text-align: left;
+      line-height: $primary-leading;
+      margin: 0;
+    }
+  }
+  .image-container:hover .info-container{
+    top: 50%;
+    left: 50%;
+    opacity: 1;
+  }
   // ANIMATIONS
+  .fadeIn-bottom{
+  top: 80%;
+  }
   .fadeUp-enter-active, .fadeUp-leave-active {
     transition: opacity 1s, transform 1s;
   }
   .fadeUp-enter, .fadeUp-leave-to {
     opacity: 0;
     transform: translateY(30%);
+  }
+  .fadeIn-enter-active, .fadeIn-leave-active {
+    transition: opacity .5s;
+  }
+  .fadeIn-enter, .fadeIn-leave-to {
+    opacity: 0;
   }
 </style>
