eaf3b38513088af3689e9062b13da65ecc4062f1
Dillon Schultz <dillonschultz93@gmail.com>
2024-03-01 10:26:00 -0800

    Refactor to include an optional 'target' prop

--- site/app/components/Link-Button.tsx
+++ site/app/components/Link-Button.tsx
@@ -5,11 +5,12 @@ export type LinkButtonProps = {
   type: 'primary' | 'neutral';
   download?: boolean;
   children: React.ReactNode;
+  target?: '_blank' | '_self' | '_parent' | '_top';
 };
 
-export const LinkButton: FunctionComponent<LinkButtonProps> = ({ href, children, type, download }) => {
+export const LinkButton: FunctionComponent<LinkButtonProps> = ({ href, children, type, download, target }) => {
   return (
-    <a href={href} className={`link-button_${type}`} target="_blank" rel="noreferrer" download={download}>
+    <a href={href} className={`link-button_${type}`} target={target ? target : '_blank'} rel="noreferrer" download={download}>
       { children }
     </a>
   );
