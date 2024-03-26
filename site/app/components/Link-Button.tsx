088e302171d87c3ab430d52da3655326790fe369
Dillon Schultz <dillonschultz93@gmail.com>
2024-03-26 16:38:11 -0700

    Restructure link component

--- site/app/components/Link-Button.tsx
+++ site/app/components/Link-Button.tsx
@@ -5,13 +5,14 @@ export type LinkButtonProps = {
   type: 'primary' | 'neutral';
   download?: boolean;
   children: React.ReactNode;
+  label: string;
   target?: '_blank' | '_self' | '_parent' | '_top';
 };
 
-export const LinkButton: FunctionComponent<LinkButtonProps> = ({ href, children, type, download, target }) => {
+export const LinkButton: FunctionComponent<LinkButtonProps> = ({ href, children, type, download, target, label }) => {
   return (
     <a href={href} className={`link-button_${type}`} target={target ? target : '_blank'} rel="noreferrer" download={download}>
-      { children }
+      {children}<span>{label}</span>
     </a>
   );
 };
