a8e8539810bff7d1a2389bde4dbb73fd136888cb
Dillon Schultz <dillonschultz93@gmail.com>
2025-01-17 18:08:58 -0800

    Add a zoomed image package

--- site/app/components/Project.tsx
+++ site/app/components/Project.tsx
@@ -4,48 +4,49 @@ import { PortableText } from "@portabletext/react";
 import imageUrlBuilder from "@sanity/image-url";
 import { getImageDimensions } from "@sanity/asset-utils";
 import { projectId, dataset } from "../sanity/projectDetails";
+import Zoom from 'react-medium-image-zoom';
 
 const builder = imageUrlBuilder({ projectId, dataset });
 
 // Bare-bones lazy-loaded image component
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const SampleImageComponent = ({ value, isInline }: { value: any, isInline: boolean }) => {
+const SimpleInlineImage = ({ value, isInline }: { value: any, isInline: boolean }) => {
   const {width, height} = getImageDimensions(value)
   return (
-    <img
-      src={builder
-        .image(value)
-        .fit('max')
-        .auto('format')
-        .quality(80)
-        .url()}
-      alt={value.alt || ' '}
-      loading="lazy"
-      style={{
-        // Display alongside text if image appears inside a block text span
-        display: isInline ? 'inline-block' : 'block',
+    <Zoom wrapElement="span">
+      <img
+        src={builder
+          .image(value)
+          .fit('max')
+          .auto('format')
+          .quality(85)
+          .url()}
+        alt={value.alt || ' '}
+        loading="lazy"
+        style={{
+          // Display alongside text if image appears inside a block text span
+          display: isInline ? 'inline-block' : 'block',
 
-        // Avoid jumping around with aspect-ratio CSS property
-        aspectRatio: width / height,
-      }}
-    />
+          // Avoid jumping around with aspect-ratio CSS property
+          aspectRatio: width / height,
+        }}
+      />
+      {/* Display a caption below the image */}
+      <span className="image-caption">
+        {value.caption}
+      </span>
+    </Zoom>
   )
 }
 
 const components = {
   types: {
-    image: SampleImageComponent,
-    // Any other custom types you have in your content
-    // Examples: mapLocation, contactForm, code, featuredProjects, latestNews, etc.
+    image: SimpleInlineImage
   },
 }
 
 export default function Project({ post }: { post: SanityDocument }) {
   const { title, description, body, mainImage } = post;
-
-  // Parse the date into a human-readable format
-  // const date = new Date(publishedAt);
-  // const formattedDate = date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
 
   return (
     <>
@@ -53,12 +54,13 @@ export default function Project({ post }: { post: SanityDocument }) {
           <img
             src={builder.image(mainImage).quality(100).url()}
             alt={title}
+            loading="lazy"
             className="banner-image col-span-full object-cover mb-8 md:mb-16 w-full md:col-start-5 lg:col-start-5"
           />
         ) : null}
       <main className={`flex flex-col gap-y-6 pb-12 max-w-4xl ${mainImage ? 'pt-0' : 'pt-48'} md:col-span-7 md:col-start-5 ${mainImage ? 'md:pb-48' : 'md:py-48'} lg:col-span-7 lg:col-start-5`}>
         {title ? (
-          <section className="flex flex-col gap-y-6 mb-16">
+          <section className="flex flex-col gap-y-6">
             <h1 className="display-lg">
               {title}
             </h1>
